c337ed2c7c535c558bd5c811ba410070c39222fc
ibrahimeraslan <ibrahimeraslan07@gmail.com>
2016-11-07 00:28:09 +0200

    Laravel üzerinden kullanıcı giriş apisinin oluşturulması

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\User;
+use Input;
+use Hash;
+use Auth;
+use Response;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class UserController extends Controller
+{
+
+    protected function login(){
+        if (Auth::attempt(array('email' => Input::get('mail'), 'password' => Input::get('sifre')))){
+            return Response::json( [
+                'trpoll' => [
+                    'case' => 1,
+                    'user_id' => Auth::user()->id,
+                    'message' => Auth::user()->api_token,
+                ]
+            ], 200);
+        }
+        else {
+            return Response::json( [
+                'trpoll' => [
+                    'case' => 0,
+                    'message' => "Mail adresiniz veya şifreniz hatalı",
+                ]
+            ], 401 );
+        }
+
+    }
+    protected function bilgiCek()
+    {
+        return Response::json( [
+            'trpoll' => [
+                'case' => 1,
+                'message' => User::where('api_token',Input::get('token'))->first(),
+            ]
+        ], 200);
+    }
+
+}
