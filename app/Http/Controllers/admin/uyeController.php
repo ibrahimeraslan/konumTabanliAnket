f45b047dc1674727c8c97d4f45481669c495a8e1
ibrahimeraslan <ibrahimeraslan07@gmail.com>
2016-12-21 23:59:34 +0200

    Üye yönetimi sayfasının kodlanması(Üye bilgilerini görme, Üyelerin katıldıkları anketleri görme, Üyelerin kazanç geçmişlerini görüntüleme, üye sil)

--- app/Http/Controllers/admin/uyeController.php
+++ app/Http/Controllers/admin/uyeController.php
@@ -2,8 +2,15 @@
 
 namespace App\Http\Controllers\admin;
 
+use App\DoldurulmusAnket;
+use App\SistemAyar;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Input;
+use Redirect;
+use Validator;
+use DB;
 
 class uyeController extends Controller
 {
@@ -14,7 +21,7 @@ class uyeController extends Controller
      */
     public function index()
     {
-        return view('admin.uye');
+        return view('admin.uye',['uyeler'=>User::all()]);
     }
 
     /**
@@ -46,7 +53,53 @@ class uyeController extends Controller
      */
     public function show($id)
     {
-        //
+        if(User::where('id',$id)->first()->tip == 1){
+            $string = "<h4>Katıldığım Anketler</h4><table class=\"table table-condensed\">
+                    <tbody><tr>
+                        <th style=\"width: 10px\">#</th>
+                        <th>Anket Adı</th>
+                        <th>Ücret</th>
+                        <th>Durum</th>
+                    </tr>";
+            $doldurulmusAnket = DB::table('doldurulmus_anketler')
+                ->join('anketler', 'anketler.id', '=', 'doldurulmus_anketler.anket_id')
+                ->where('doldurulmus_anketler.uye_id', '=', $id)
+                ->get();
+
+            foreach ($doldurulmusAnket as $d){
+                $ucret = $d->anket_ucret-($d->anket_ucret*(SistemAyar::first()->ucret_kesintisi/100));
+                if($d->durum==0)
+                    $durum = '<span class="label label-warning">Kontrol Ediliyor</span>';
+                elseif($d->durum==1)
+                    $durum = '<span class="label label-danger">Red Edildi!</span>';
+                elseif($d->durum==2)
+                    $durum = '<span class="label label-tertiary">Onaylandı</span>';
+                $string .= '<tr><td>'.$d->id.'</td><td>'.$d->anket_adi.'</td><td>'.$ucret.'TL</td><td>'.$durum.'</td></tr>';
+            }
+        }else{
+            $string = "<h4>Oluşturduğum Anketler</h4><table class=\"table table-condensed\">
+                    <tbody><tr>
+                        <th style=\"width: 10px\">#</th>
+                        <th>Anket Adı</th>
+                        <th>Katılım Limiti</th>
+                        <th>Doldurma Başına Ücret</th>
+                        <th>Katılmış Kişi Sayısı</th>
+                        <th>Anket Durumu</th>
+                    </tr>";
+            $anketler = DB::table('anketler')->where('uye_id',$id)->get();
+
+            foreach ($anketler as $d){
+
+                if($d->anket_durum==0)
+                    $durum = '<span class="label label-danger">Yayında değil</span>';
+                elseif($d->anket_durum==1)
+                    $durum = '<span class="label label-success">Yayında</span>';
+
+                $string .= '<tr><td>'.$d->id.'</td><td>'.$d->anket_adi.'</td><td>'.$d->anket_limit.'</td><td>'.$d->anket_ucret.'TL</td><td>'.$d->anket_katilim_sayisi.'</td><td>'.$durum.'</td></tr>';
+            }
+        }
+
+        return $string;
     }
 
     /**
@@ -69,7 +122,33 @@ class uyeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $rules = array(
+            'yetki'  => 'required|numeric',
+        );
+        $validator = Validator::make(Input::all(), $rules);
+        if ($validator->fails()) {
+            $messages = $validator->messages();
+            return Redirect::back()
+                ->withErrors($validator);
+        } else {
+            if(Input::get('yetki')==1){
+                User::where('id',$id)->update([
+                    'is_admin'=>0
+                ]);
+                return Redirect::back()
+                    ->with('status','Yetki başarıyla ayarlanmıştır.');
+            }else if(Input::get('yetki')==2){
+                User::where('id',$id)->update([
+                    'is_admin'=>1
+                ]);
+                return Redirect::back()
+                    ->with('status','Yetki başarıyla ayarlanmıştır.');
+            }
+            else{
+                return Redirect::back()
+                    ->withErrors("Hatalı işlem");
+            }
+        }
     }
 
     /**
@@ -80,6 +159,12 @@ class uyeController extends Controller
      */
     public function destroy($id)
     {
-        //
+        User::where('id',$id)->delete();
+        DoldurulmusAnket::where('uye_id',$id)->delete();
+        DB::table('para_aktarim_istekleri')->where('user_id',$id)->delete();
+        DB::table('uyelik_soru_cevaplari')->where('uye_id',$id)->delete();
+
+        return Redirect::back()
+            ->with('status','Üye Başarıyla Silinmiştir.');
     }
 }
--- resources/views/admin/uye.blade.php
+++ resources/views/admin/uye.blade.php
@@ -24,27 +24,51 @@
                         <th>Tip</th>
                         <th>İşlemler</th>
                     </tr>
+                    @foreach($uyeler as $i=>$uye)
                     <tr>
-                        <td>1.</td>
-                        <td><img src=""></td>
-                        <td>İbrahim Eraslan</td>
-                        <td>eraslan.07@gmail.com</td>
+                        <td>{{ $i+1 }}</td>
+                        <td><img style="max-width:40px; height:auto; border-radius: 100%" src="{{ asset('img/avatar/'.$uye->avatar) }}"></td>
+                        <td>{{ $uye->name }}</td>
+                        <td>{{ $uye->email }}</td>
                         <td>
+                            @if($uye->is_admin == 1)
                             <span class="badge bg-red">Admin</span>
+                            @else
                             <span class="badge bg-green">Üye</span>
+                            @endif
                         </td>
                         <td>
+                            @if($uye->tip == 2)
                             <span class="badge bg-red">Anket Veren</span>
+                            @else
                             <span class="badge bg-green">Anket Dolduran</span>
+                            @endif
                         </td>
+                        <form name="sil{{ $uye->id }}" method="post" action="{{ URL::to('admin/uye/'.$uye->id) }}">
+                            <input type="hidden" name="_method" value="DELETE">
+                            {{ csrf_field() }}
+                        </form>
+                        <form name="adminle{{ $uye->id }}" method="post" action="{{ URL::to('admin/uye/'.$uye->id) }}">
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="yetki" value="2">
+                            {{ csrf_field() }}
+                        </form>
+                        <form name="uyele{{ $uye->id }}" method="post" action="{{ URL::to('admin/uye/'.$uye->id) }}">
+                            <input type="hidden" name="_method" value="PUT">
+                            <input type="hidden" name="yetki" value="1">
+                            {{ csrf_field() }}
+                        </form>
                         <td>
-                            <a class="btn btn-xs btn-warning" title="Admin Yetkisi Ver"><i class="fa  fa-star"></i></a>
-                            <a class="btn btn-xs btn-warning" title="Üye Yetkisi Ver"><i class="fa  fa-star-o"></i></a>
-                            <a class="btn btn-xs btn-info" title="Üye Detayları"><i class="fa fa-eye"></i></a>
-                            <a class="btn btn-xs btn-danger" title="Üyeyi Sil"><i class="fa fa-remove"></i></a>
+                            @if($uye->is_admin == 0)
+                            <a onclick="adminle{{ $uye->id }}.submit()" class="btn btn-xs btn-warning" title="Admin Yetkisi Ver"><i class="fa  fa-star"></i></a>
+                            @else
+                            <a onclick="uyele{{ $uye->id }}.submit()" class="btn btn-xs btn-warning" title="Üye Yetkisi Ver"><i class="fa  fa-star-o"></i></a>
+                            @endif
+                            <a class="btn btn-xs btn-info" data-toggle="modal" data-target="#uyeBilgileri" onclick="uyeBilgi({{ $uye->id }})" title="Üye Detayları"><i class="fa fa-eye"></i></a>
+                            <a class="btn btn-xs btn-danger" onclick="sil{{ $uye->id }}.submit()" title="Üyeyi Sil"><i class="fa fa-remove"></i></a>
                         </td>
                     </tr>
-
+                    @endforeach
                     </tbody></table>
             </div>
         </div>
@@ -53,4 +77,34 @@
 </div>
 
     </section>
+
+    <div class="modal fade" id="uyeBilgileri" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title" id="myModalLabel">Üye Bilgileri</h4>
+                </div>
+                <div class="modal-body">
+                    <div id="uyeLoading"></div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Kapat</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<script>
+    function uyeBilgi(id){
+        $('#uyeLoading').html('<center><img src="/template/img/progress-circle-master.svg"></center>');
+        $.ajax({
+            type:'POST',
+            data:"_method=GET&id="+id,
+            url:'{{ URL::to('admin/uye/') }}/'+id,
+            success:function(gelen){
+                $('#uyeLoading').html(gelen);
+            }
+        });
+    }
+</script>
 @endsection
